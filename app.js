80a6676c2e1007dbbca18c76c540370b61fb76fc
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-03-22 23:32:28 -0600

    Deleted jquery, startedover

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-var playerName1 = "" , playerName2 = "" , turn = "";
-var grid = [[0,0,0], [0,0,0], [0,0,0]];
-var hasWinner = 0, moveCount = 0;
-
-function boardMsg(x) {
-  console.log(x);
-  return $("board").text(x);
-  
-}
-
-function setTurn() {
-
-  var r = Math.floor((Math.random() * 2) + 1);
-  hasWinner = 0;
-  if (r == 1) {
-    turn = playerName1;
-    boardMsg(playerName1 + "'s turn now!");
-  }
-  else {
-    turn = playerName2;
-    boardMsg(playerName2 + "'s turn now!");
-  }
-  
+var player1 = player1;
+var player2 = player2;
 
 
-}
 
-setTurn();
