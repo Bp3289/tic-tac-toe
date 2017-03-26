17e6ad30dfe922afb6bec129bda9655dbbfd839d
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-03-26 13:18:23 -0600

    Add checkForWinner function with all possible outcomes

--- app.js
+++ app.js
@@ -26,12 +26,23 @@ function startGame(){
 		setMessage("It's" + document.turn + "'s turn!");
 	}
 
+	function checkForWinner(move) {
+		var result = flase;
+		if (check(1, 2, 3, move) || check(4, 5, 6, move) || check(7, 8, 9, move) || check(1, 4, 7, move) || check(2, 5, 8, move) || check(3, 6, 9, move) || check(1, 5, 9, move) || check(3, 5, 7, move)) {
+			result = true;
+		}
+		return result;
+	}
+
 
 	function check(a, b, c, move) {
 		var result = flase;
-		
+		if (getBox(a) === move && getBox(b) === getBox(c) === move) {
+			result = true;
+		}
+		return result;
 	}
 
 	function getBox(number) {
-		
+	return	document.getElementById("s" + number).innerText;
 	}
