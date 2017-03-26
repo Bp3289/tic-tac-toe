ceae578b85f78090da49f76159d0a7828545a831
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-03-26 12:54:57 -0600

    Added JS functions for start game and switchTurn

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-var player1 = player1;
-var player2 = player2;
+function startGame(){
+		document.turn = "X";
 
+		setMessage(document.turn + " gets to start.");
+	}
 
+	function setMessage(msg) {
+		document.getElementById('message').innerText = msg;
+	}
 
+	function nextMove(square) {
+		square.innerText = document.turn;
+		switchTurn();
+
+	}
+
+	function switchTurn() {
+		if (document.turn =="X") {
+			document.turn = "O";
+		} else {
+			document.turn = "X";
+		}
+	}
